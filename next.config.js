3d5747d7c2b7bd3c2f79f5e23fc5f99c7241ed99
madcjmac <andor.adept@gmail.com>
2025-07-30 13:46:36 +0100

    Add next.config.js - Multi-Agent AI

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/media/[name][ext]'
+      }
+    });
+    return config;
+  }
+}
